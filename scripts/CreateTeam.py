acfd1ff238ad56c4d182c942bcf04a36c1bc6ba8
kengraf <ken@securityxing.com>
2017-09-07 21:09:36 -0400

    start school automation

--- scripts/CreateTeam.py
+++ scripts/CreateTeam.py
@@ -1,5 +1,7 @@
 # Create a new blue team
 import os
+import getopt
+import sys
 import boto3
 
 # Predetermined values specific to 2018
@@ -7,14 +9,16 @@ PARENT_ID = 'ou-ej2m-9e1lj6fx'
 
 # Default input values
 TEAM_NAME = 'team0.3'
-TEAM_EMAIL = teamName + '@neccdc2018.org'
+TEAM_EMAIL = TEAM_NAME + '@neccdc2018.org'
 TEAM_CIDR = "132.177.0.0/16"
 
 def main():
-# Read command line args
-opts, args = getopt.getopt(sys.argv[1:],"n:c:e:",["name=","cidr=","email="])
+  global TEAM_NAME, TEAM_EMAIL
+  
+  # Read command line args
+  opts, args = getopt.getopt(sys.argv[1:],"n:c:e:",["name=","cidr=","email="])
 
-for o, a in opts:
+  for o, a in opts:
     if o in ( '-n','--name'):
         TEAM_NAME = a
     elif o in ( '-e','--email'):
@@ -24,17 +28,17 @@ for o, a in opts:
     else:
         print("Usage: %s -n name -e email -c cidr" % sys.argv[0])
         
-    session = boto3.Session(profile_name='neccdc', region_name='us-east-1')
-    client = session.client('organizations')
+  session = boto3.Session(profile_name='neccdc', region_name='us-east-1')
+  client = session.client('organizations')
     
-    response = client.create_organizational_unit(ParentId=PARENT_ID, Name=TEAM_NAME)
-    
-    response = client.create_account(Email='',
-                                     AccountName='',
+  response = client.create_organizational_unit(ParentId=PARENT_ID, Name=TEAM_NAME)
+  print (response)
+
+  response = client.create_account(Email=TEAM_EMAIL,
+                                     AccountName=TEAM_NAME,
                                      RoleName='OrganizationAccountAccessRole',
                                      IamUserAccessToBilling='DENY')
-        
+  print (response)    
 
-if __name__ == '__main__':
-    main()
+main()
     
